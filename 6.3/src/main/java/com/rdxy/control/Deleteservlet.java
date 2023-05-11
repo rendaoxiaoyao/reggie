ec348ab28832a26bb2893ac4ebdc927e9cb027d2
人道逍遥 <3538993644@qq.com>
2023-05-11 16:35:35 +0800

    导入6.3

---
+++ 6.3/src/main/java/com/rdxy/control/Deleteservlet.java
@@ -0,0 +1,36 @@
+package com.rdxy.control;
+
+import com.rdxy.model.StudentModel;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class Deleteservlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public Deleteservlet() {
+		super();
+	}
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
+       this.doPost(request, response);
+    }
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+     		request.setCharacterEncoding("gbk");
+     		int id=Integer.parseInt(request.getParameter("id"));
+     		StudentModel model = new StudentModel();
+	 	    model.delete(id);
+	 	    response.sendRedirect("ListStudentServlet.do");
+	}
+
+}
