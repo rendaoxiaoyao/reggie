f660e98a7bdde82df66baffadc8c343acb6473e7
人道逍遥 <3538993644@qq.com>
2023-05-12 12:18:20 +0800

    6.3 学生头像

--- 6.3/src/main/java/com/rdxy/filter/StudentFilter.java
+++ 6.3/src/main/java/com/rdxy/filter/StudentFilter.java
@@ -17,7 +17,7 @@ public class StudentFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException, IOException {
-        //servletRequest.setCharacterEncoding("UTF-8");
+        servletRequest.setCharacterEncoding("UTF-8");
 
         System.out.println("这里是doFilter");
 
